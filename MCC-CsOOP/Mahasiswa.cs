037cc60cec63790ff47a2ee1a43bc21cc522b218
Syed Abdul Jabar <7abdjabar@gmail.com>
2021-12-17 21:03:28 +0700

    Create Mahasiswa.cs

---
+++ MCC-CsOOP/Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCC_CsOOP
+{
+    public class Mahasiswa
+    {
+        public String nameMhs { get; set; }
+        public String nimMhs { get; set; }
+        public int examValMhs { get; set; }
+
+        public Mahasiswa(string nameMhs, string nimMhs, int examValMhs)
+        {
+            this.nameMhs = nameMhs;
+            this.nimMhs = nimMhs;
+            this.examValMhs = examValMhs;
+        }
+
+        public Mahasiswa(int examValMhs)
+        {
+            this.examValMhs = examValMhs;
+        }
+
+        public Mahasiswa()
+        {
+        }
+    }
+}
