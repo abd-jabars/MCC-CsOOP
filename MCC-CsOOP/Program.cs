a88a9b508f120e2f9883c01670a29d9cd745bea5
Syed Abdul Jabar <7abdjabar@gmail.com>
2021-12-17 19:53:20 +0700

    Add project files.

---
+++ MCC-CsOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCC_CsOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
